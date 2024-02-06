e8fc1554936a773b3b27fcaec956240efcba24f4
nmb735 <102815981+nmb735@users.noreply.github.com>
2024-02-06 16:14:15 +0100

    Update connectionBD.php

--- model/connectionBD.php
+++ model/connectionBD.php
@@ -1,9 +1,9 @@
 <?php
 function connectionBD(){
-    $servername = "localhost";
-    $username = "tdiw-d3";
-    $password = "a38ZEq9p";
-    $dbname = "tdiw-d3";
+    $servername = "servername";
+    $username = "username";
+    $password = "password";
+    $dbname = "dbname";
     try{
         $conn = new PDO("pgsql:host=$servername;port=5432;dbname=$dbname", $username, $password);
         return $conn;
